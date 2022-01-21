26668c2cb716ef4136ca561667d743b5a26b0e84
matt21ferg <matt21ferguson@gmail.com>
2022-01-21 10:56:21 -0600

    1st commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+
+        
+        }
+        public double Balance 
+        {
+            get { return _balance; }
+
+
+            set { _balance = value; } 
+        }
+
+
+
+
+        private double _balance = 0;
+        public double AccountBalance()
+        {
+            return _balance;
+        }
+        public double Deposit(double Amount)
+        {
+            return Balance += Amount;
+        }
+        public double Withdraw(double Amount)
+        { return Balance -= Amount; }
+    
+    
+    
+    }
+
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var MyBankAccount = new BankAccount();
+            MyBankAccount.Deposit(39);
+            Console.WriteLine($" your balance is {MyBankAccount.Balance}");
+            Console.WriteLine( );
+            MyBankAccount.Withdraw(20);
+            Console.WriteLine($" your balance is {MyBankAccount.Balance}");
+
+        }
+    }
+}
